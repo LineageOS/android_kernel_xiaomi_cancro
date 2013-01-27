3b4e3a21b1367b093c29b3971e264de8077ee299
imoseyon <imoseyon@gmail.com>
2013-01-27 16:24:25 -0600

    Linux 3.4.31 - 3.4.40
    Conflicts:
    
    	net/ipv4/ping.c

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -844,7 +844,8 @@ restart:
 	dst_hold(&rt->dst);
 	read_unlock_bh(&table->tb6_lock);
 
-	if (!dst_get_neighbour_noref_raw(&rt->dst) && !(rt->rt6i_flags & RTF_NONEXTHOP))
+	if (!dst_get_neighbour_noref_raw(&rt->dst) &&
+	    !(rt->rt6i_flags & (RTF_NONEXTHOP | RTF_LOCAL)))
 		nrt = rt6_alloc_cow(rt, &fl6->daddr, &fl6->saddr);
 	else if (!(rt->dst.flags & DST_HOST))
 		nrt = rt6_alloc_clone(rt, &fl6->daddr);
@@ -1964,6 +1965,27 @@ int rt6_addrconf_purge(struct rt6_info *rt, void *arg) {
 void rt6_purge_dflt_routers(struct net *net)
 {
 	fib6_clean_all(net, rt6_addrconf_purge, 0, NULL);
+
+	struct rt6_info *rt;
+	struct fib6_table *table;
+
+	/* NOTE: Keep consistent with rt6_get_dflt_router */
+	table = fib6_get_table(net, RT6_TABLE_DFLT);
+	if (!table)
+		return;
+
+restart:
+	read_lock_bh(&table->tb6_lock);
+	for (rt = table->tb6_root.leaf; rt; rt = rt->dst.rt6_next) {
+		if (rt->rt6i_flags & (RTF_DEFAULT | RTF_ADDRCONF) &&
+		    (!rt->rt6i_idev || rt->rt6i_idev->cnf.accept_ra != 2)) {
+			dst_hold(&rt->dst);
+			read_unlock_bh(&table->tb6_lock);
+			ip6_del_rt(rt);
+			goto restart;
+		}
+	}
+	read_unlock_bh(&table->tb6_lock);
 }
 
 static void rtmsg_to_fib6_config(struct net *net,
