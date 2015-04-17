5453d8963c736da2160ba3e06d35e495685c035b
Shivaraj Shetty <shivaraj@codeaurora.org>
2015-04-18 00:11:22 +0530

    msm: mdss: protect iommu operations with mutex for mdp3
    On mdp3 targets iommu operation is not properly protected
    by mutex lock. This is causing iommu to be detached while
    hardware is fetching the data from the iommu mapped
    address leading to SNOC errors. Use mutex lock properly
    while using the iommu to fix the issue.
    
    Change-Id: Ife59791b08b123f6b60d3ffa6d8061839244934c
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/msm/mdss/mdp3.c
+++ drivers/video/msm/mdss/mdp3.c
@@ -958,8 +958,15 @@ static int mdp3_res_init(void)
 
 static void mdp3_res_deinit(void)
 {
+	int i;
+
 	mdp3_bus_scale_unregister();
-	mdp3_iommu_dettach(MDP3_IOMMU_CTX_DMA_0);
+
+	mutex_lock(&mdp3_res->iommu_lock);
+	for (i = 0; i < MDP3_IOMMU_CTX_MAX; i++)
+		mdp3_iommu_dettach(i);
+	mutex_unlock(&mdp3_res->iommu_lock);
+
 	mdp3_iommu_deinit();
 
 	if (!IS_ERR_OR_NULL(mdp3_res->ion_client))
@@ -1659,12 +1666,14 @@ int mdp3_iommu_enable(int client)
 {
 	int rc;
 
+	mutex_lock(&mdp3_res->iommu_lock);
 	if (client == MDP3_CLIENT_DMA_P) {
 		rc = mdp3_iommu_attach(MDP3_IOMMU_CTX_DMA_0);
 	} else {
 		rc = mdp3_iommu_attach(MDP3_IOMMU_CTX_PPP_0);
 		rc |= mdp3_iommu_attach(MDP3_IOMMU_CTX_PPP_1);
 	}
+	mutex_unlock(&mdp3_res->iommu_lock);
 
 	return rc;
 }
@@ -1673,12 +1682,14 @@ int mdp3_iommu_disable(int client)
 {
 	int rc;
 
+	mutex_lock(&mdp3_res->iommu_lock);
 	if (client == MDP3_CLIENT_DMA_P) {
 		rc = mdp3_iommu_dettach(MDP3_IOMMU_CTX_DMA_0);
 	} else {
 		rc = mdp3_iommu_dettach(MDP3_IOMMU_CTX_PPP_0);
 		rc |= mdp3_iommu_dettach(MDP3_IOMMU_CTX_PPP_1);
 	}
+	mutex_unlock(&mdp3_res->iommu_lock);
 
 	return rc;
 }
