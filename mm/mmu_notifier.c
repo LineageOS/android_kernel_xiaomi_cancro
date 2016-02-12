bce569567c9bb89b71a6a4df8c582e38680fc52a
tamirda <smstamir@gmail.com>
2016-02-13 01:53:57 +0200

    Linux 3.4.48

--- mm/mmu_notifier.c
+++ mm/mmu_notifier.c
@@ -37,6 +37,7 @@ static struct srcu_struct srcu;
 void __mmu_notifier_release(struct mm_struct *mm)
 {
 	struct mmu_notifier *mn;
+<<<<<<< HEAD
 	int id;
 
 	/*
@@ -46,6 +47,27 @@ void __mmu_notifier_release(struct mm_struct *mm)
 	 * returned.
 	 */
 	id = srcu_read_lock(&srcu);
+=======
+	struct hlist_node *node;
+	int id;
+
+	/*
+	 * SRCU here will block mmu_notifier_unregister until
+	 * ->release returns.
+	 */
+	id = srcu_read_lock(&srcu);
+	hlist_for_each_entry_rcu(mn, node, &mm->mmu_notifier_mm->list, hlist)
+		/*
+		 * If ->release runs before mmu_notifier_unregister it must be
+		 * handled, as it's the only way for the driver to flush all
+		 * existing sptes and stop the driver from establishing any more
+		 * sptes before all the pages in the mm are freed.
+		 */
+		if (mn->ops->release)
+			mn->ops->release(mn, mm);
+	srcu_read_unlock(&srcu, id);
+
+>>>>>>> 59598aa... Linux 3.4.48
 	spin_lock(&mm->mmu_notifier_mm->lock);
 	while (unlikely(!hlist_empty(&mm->mmu_notifier_mm->list))) {
 		mn = hlist_entry(mm->mmu_notifier_mm->list.first,
@@ -53,8 +75,15 @@ void __mmu_notifier_release(struct mm_struct *mm)
 				 hlist);
 
 		/*
+<<<<<<< HEAD
 		 * Unlink.  This will prevent mmu_notifier_unregister()
 		 * from also making the ->release() callout.
+=======
+		 * We arrived before mmu_notifier_unregister so
+		 * mmu_notifier_unregister will do nothing other than to wait
+		 * for ->release to finish and for mmu_notifier_unregister to
+		 * return.
+>>>>>>> 59598aa... Linux 3.4.48
 		 */
 		hlist_del_init_rcu(&mn->hlist);
 		spin_unlock(&mm->mmu_notifier_mm->lock);
@@ -70,6 +99,7 @@ void __mmu_notifier_release(struct mm_struct *mm)
 	spin_unlock(&mm->mmu_notifier_mm->lock);
 
 	/*
+<<<<<<< HEAD
 	 * All callouts to ->release() which we have done are complete.
 	 * Allow synchronize_srcu() in mmu_notifier_unregister() to complete
 	 */
@@ -82,6 +112,15 @@ void __mmu_notifier_release(struct mm_struct *mm)
 	 * to ensure that all notifier callouts for this mm have been
 	 * completed and the sptes are really cleaned up before returning
 	 * to exit_mmap().
+=======
+	 * synchronize_srcu here prevents mmu_notifier_release from returning to
+	 * exit_mmap (which would proceed with freeing all pages in the mm)
+	 * until the ->release method returns, if it was invoked by
+	 * mmu_notifier_unregister.
+	 *
+	 * The mmu_notifier_mm can't go away from under us because one mm_count
+	 * is held by exit_mmap.
+>>>>>>> 59598aa... Linux 3.4.48
 	 */
 	synchronize_srcu(&srcu);
 }
@@ -304,10 +343,23 @@ void mmu_notifier_unregister(struct mmu_notifier *mn, struct mm_struct *mm)
 
 	spin_lock(&mm->mmu_notifier_mm->lock);
 	if (!hlist_unhashed(&mn->hlist)) {
+<<<<<<< HEAD
+=======
+		/*
+		 * SRCU here will force exit_mmap to wait for ->release to
+		 * finish before freeing the pages.
+		 */
+>>>>>>> 59598aa... Linux 3.4.48
 		int id;
 
+		id = srcu_read_lock(&srcu);
 		/*
+<<<<<<< HEAD
 		 * Ensure we synchronize up with __mmu_notifier_release().
+=======
+		 * exit_mmap will block in mmu_notifier_release to guarantee
+		 * that ->release is called before freeing the pages.
+>>>>>>> 59598aa... Linux 3.4.48
 		 */
 		id = srcu_read_lock(&srcu);
 
@@ -316,17 +368,33 @@ void mmu_notifier_unregister(struct mmu_notifier *mn, struct mm_struct *mm)
 
 		if (mn->ops->release)
 			mn->ops->release(mn, mm);
+<<<<<<< HEAD
 
 		/*
 		 * Allow __mmu_notifier_release() to complete.
 		 */
 		srcu_read_unlock(&srcu, id);
 	} else
+=======
+		srcu_read_unlock(&srcu, id);
+
+		spin_lock(&mm->mmu_notifier_mm->lock);
+		/*
+		 * Can not use list_del_rcu() since __mmu_notifier_release
+		 * can delete it before we hold the lock.
+		 */
+		hlist_del_init_rcu(&mn->hlist);
+>>>>>>> 59598aa... Linux 3.4.48
 		spin_unlock(&mm->mmu_notifier_mm->lock);
 
 	/*
+<<<<<<< HEAD
 	 * Wait for any running method to finish, including ->release() if it
 	 * was run by __mmu_notifier_release() instead of us.
+=======
+	 * Wait for any running method to finish, of course including
+	 * ->release if it was run by mmu_notifier_relase instead of us.
+>>>>>>> 59598aa... Linux 3.4.48
 	 */
 	synchronize_srcu(&srcu);
 
@@ -342,3 +410,7 @@ static int __init mmu_notifier_init(void)
 }
 
 module_init(mmu_notifier_init);
+<<<<<<< HEAD
+=======
+
+>>>>>>> 59598aa... Linux 3.4.48
