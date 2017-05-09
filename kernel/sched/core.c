24930cfec7e4b7b360e99a67cd19a44253bb54c1
sanekghost <daniheras@live.com>
2017-05-09 16:16:06 +0200

    fix compile linux

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1685,7 +1685,7 @@ static void try_to_wake_up_local(struct task_struct *p)
 	if (WARN_ON_ONCE(rq != this_rq()) ||
 	    WARN_ON_ONCE(p == current))
 		return;
-	}
+	
 
 	lockdep_assert_held(&rq->lock);
 
--- mm/vmscan.c
+++ mm/vmscan.c
@@ -671,7 +671,7 @@ static enum page_references page_check_references(struct page *page,
 		return PAGEREF_RECLAIM;
 
 	if (referenced_ptes) {
-		if (PageSwapBacked(page))
+		if (PageAnon(page))
 			return PAGEREF_ACTIVATE;
 		/*
 		 * All mapped pages start out with page table
@@ -1751,8 +1751,8 @@ out:
 		unsigned long scan;
 
 		scan = zone_nr_lru_pages(mz, lru);
-		if (priority || noswap || !vmscan_swappiness(mz, sc)) {
-			scan >>= priority;
+		if (sc->priority || noswap || !vmscan_swappiness(sc)) {
+ 			scan >>= sc->priority;
 			if (!scan && force_scan)
 				scan = SWAP_CLUSTER_MAX;
 			scan = div64_u64(scan * fraction[file], denominator);
@@ -2760,8 +2760,16 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int order, int classzone_idx)
 		 * per-cpu vmstat threshold while kswapd is awake and restore
 		 * them before going back to sleep.
 		 */
+		 
 		set_pgdat_percpu_threshold(pgdat, calculate_normal_threshold);
-
+		
+         /*
+ 		 * Compaction records what page blocks it recently failed to
+ 		 * isolate pages from and skips them in the future scanning.
+ 		 * When kswapd is going to sleep, it is reasonable to assume
+ 		 * that pages and compaction may succeed so reset the cache.
+ 		 */
+ 		reset_isolation_suitable(pgdat);
 		if (!kthread_should_stop())
 			schedule();
 
@@ -2877,7 +2885,7 @@ static int kswapd(void *p)
 		}
 	}
 
-	current->reclaim_state = NULL;
+	
 	return 0;
 }
 
@@ -3019,17 +3027,15 @@ int kswapd_run(int nid)
 }
 
 /*
- * Called by memory hotplug when all memory in a node is offlined.  Caller must
- * hold lock_memory_hotplug().
+ * Called by memory hotplug when all memory in a node is offlined.
  */
 void kswapd_stop(int nid)
 {
 	struct task_struct *kswapd = NODE_DATA(nid)->kswapd;
 
-	if (kswapd) {
+	if (kswapd)
 		kthread_stop(kswapd);
-		NODE_DATA(nid)->kswapd = NULL;
-	}
+		
 }
 
 static int __init kswapd_init(void)
