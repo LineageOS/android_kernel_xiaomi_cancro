1e87b998c97c88e2c9e2ea6fba670bc357e6afe4
sanekghost <daniheras@live.com>
2017-05-10 03:36:17 +0200

    fix compile linux

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1687,7 +1687,7 @@ static void try_to_wake_up_local(struct task_struct *p)
 			__func__, task_pid_nr(p), p->comm, rq,
 			this_rq(), p, current);
 		return;
-	
+	}
 
 	lockdep_assert_held(&rq->lock);
 
